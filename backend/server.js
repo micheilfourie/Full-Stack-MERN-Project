a3cdde4454b1327762e53fbcf3c334efabd65198
Micheil <fouriemicheil1@gmail.com>
2025-01-09 11:09:02 +0200

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+
+app.listen(5000, () => {
+    connectDB();
+    console.log("Server is running on port 5000");
+});
+
